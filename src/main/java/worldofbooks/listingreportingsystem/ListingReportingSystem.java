ed66a0a4c52161063c67be3242d2e598250e3e89
Roland Csosz <roland@iot-inspector.com>
2021-06-10 11:28:50 +0200

    setup persistence.xml and hibernate

--- src/main/java/worldofbooks/listingreportingsystem/ListingReportingSystem.java
+++ src/main/java/worldofbooks/listingreportingsystem/ListingReportingSystem.java
@@ -1,7 +1,23 @@
 package worldofbooks.listingreportingsystem;
 
+import worldofbooks.listingreportingsystem.util.JPAUtil;
+
+import javax.persistence.EntityManager;
+
 public class ListingReportingSystem {
     public static void main(String[] args) {
+        EntityManager entityManager = JPAUtil.getEntityManagerFactory().createEntityManager();
+        entityManager.getTransaction().begin();
 
+        // Check database version
+        String sql = "select version()";
+
+        String result = (String) entityManager.createNativeQuery(sql).getSingleResult();
+        System.out.println(result);
+
+        entityManager.getTransaction().commit();
+        entityManager.close();
+
+        JPAUtil.shutdown();
     }
 }
