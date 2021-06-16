e0b60199c7bd50f702163d821c4115b95029f663
Roland Csosz <roland@iot-inspector.com>
2021-06-16 12:56:44 +0200

    Specified column types for object mapping

--- src/main/java/worldofbooks/listingreportingsystem/model/entity/Listing.java
+++ src/main/java/worldofbooks/listingreportingsystem/model/entity/Listing.java
@@ -1,9 +1,6 @@
 package worldofbooks.listingreportingsystem.model.entity;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.OneToOne;
+import javax.persistence.*;
 import java.math.BigDecimal;
 import java.util.Date;
 import java.util.UUID;
@@ -14,20 +11,138 @@ public class Listing {
     @Id
     private UUID id;
 
+    @Column(columnDefinition = "TEXT")
     private String title;
+
+    @Column(columnDefinition = "TEXT")
     private String description;
+
+    @Column(columnDefinition = "DECIMAL")
     private BigDecimal listingPrice;
+
+    @Column(columnDefinition = "TEXT")
     private String currency;
+
+    @Column(columnDefinition = "INT")
     private int quantity;
+
+    @Column(columnDefinition = "TEXT")
     private String ownerEmailAddress;
+
+    @Temporal(TemporalType.DATE)
     private Date uploadTime;
 
-    @OneToOne(cascade = {CascadeType.ALL})
-    private Location inventoryItemLocationId;
+    @ManyToOne(cascade = {CascadeType.ALL})
+    private Location location;
 
-    @OneToOne(cascade = {CascadeType.ALL})
+    @ManyToOne(cascade = {CascadeType.ALL})
     private ListingStatus listingStatus;
 
     @OneToOne(cascade = {CascadeType.ALL})
     private Marketplace marketplace;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getListingPrice() {
+        return listingPrice;
+    }
+
+    public void setListingPrice(BigDecimal listingPrice) {
+        this.listingPrice = listingPrice;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getOwnerEmailAddress() {
+        return ownerEmailAddress;
+    }
+
+    public void setOwnerEmailAddress(String ownerEmailAddress) {
+        this.ownerEmailAddress = ownerEmailAddress;
+    }
+
+    public Date getUploadTime() {
+        return uploadTime;
+    }
+
+    public void setUploadTime(Date uploadTime) {
+        this.uploadTime = uploadTime;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location inventoryItemLocationId) {
+        this.location = inventoryItemLocationId;
+    }
+
+    public ListingStatus getListingStatus() {
+        return listingStatus;
+    }
+
+    public void setListingStatus(ListingStatus listingStatus) {
+        this.listingStatus = listingStatus;
+    }
+
+    public Marketplace getMarketplace() {
+        return marketplace;
+    }
+
+    public void setMarketplace(Marketplace marketplace) {
+        this.marketplace = marketplace;
+    }
+
+    @Override
+    public String toString() {
+        return "Listing{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", listingPrice=" + listingPrice +
+                ", currency='" + currency + '\'' +
+                ", quantity=" + quantity +
+                ", ownerEmailAddress='" + ownerEmailAddress + '\'' +
+                ", uploadTime=" + uploadTime +
+                ", location=" + location +
+                ", listingStatus=" + listingStatus +
+                ", marketplace=" + marketplace +
+                '}';
+    }
 }
--- src/main/java/worldofbooks/listingreportingsystem/model/entity/Location.java
+++ src/main/java/worldofbooks/listingreportingsystem/model/entity/Location.java
@@ -1,8 +1,12 @@
 package worldofbooks.listingreportingsystem.model.entity;
 
+import com.fasterxml.jackson.annotation.JsonSetter;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.persistence.OneToOne;
+import javax.persistence.OneToMany;
+import java.util.List;
 import java.util.UUID;
 
 @Entity
@@ -11,14 +15,118 @@ public class Location {
     @Id
     private UUID id;
 
+    @Column(columnDefinition = "TEXT")
     private String managerName;
+
+    @Column(columnDefinition = "TEXT")
     private String phone;
+
+    @Column(columnDefinition = "TEXT")
     private String addressPrimary;
+
+    @Column(columnDefinition = "TEXT")
     private String addressSecondary;
+
+    @Column(columnDefinition = "TEXT")
     private String country;
+
+    @Column(columnDefinition = "TEXT")
     private String town;
+
+    @Column(columnDefinition = "TEXT")
     private String postalCode;
 
-    @OneToOne(mappedBy="inventoryItemLocationId")
-    private Listing listing;
+    @OneToMany(mappedBy= "location")
+    private List<Listing> listing;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getManagerName() {
+        return managerName;
+    }
+
+    @JsonSetter("manager_name")
+    public void setManagerName(String managerName) {
+        this.managerName = managerName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddressPrimary() {
+        return addressPrimary;
+    }
+
+    @JsonSetter("address_primary")
+    public void setAddressPrimary(String addressPrimary) {
+        this.addressPrimary = addressPrimary;
+    }
+
+    public String getAddressSecondary() {
+        return addressSecondary;
+    }
+
+    @JsonSetter("address_secondary")
+    public void setAddressSecondary(String addressSecondary) {
+        this.addressSecondary = addressSecondary;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getTown() {
+        return town;
+    }
+
+    public void setTown(String town) {
+        this.town = town;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    @JsonSetter("postal_code")
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public List<Listing> getListing() {
+        return listing;
+    }
+
+    public void setListing(List<Listing> listing) {
+        this.listing = listing;
+    }
+
+    @Override
+    public String toString() {
+        return "Location{" +
+                "id=" + id +
+                ", managerName='" + managerName + '\'' +
+                ", phone='" + phone + '\'' +
+                ", addressPrimary='" + addressPrimary + '\'' +
+                ", addressSecondary='" + addressSecondary + '\'' +
+                ", country='" + country + '\'' +
+                ", town='" + town + '\'' +
+                ", postalCode='" + postalCode + '\'' +
+                ", listing=" + listing +
+                '}';
+    }
 }
