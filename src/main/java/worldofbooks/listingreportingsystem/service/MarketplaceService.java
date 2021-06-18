1bf4d87b8f10a85e97b0a35e8772c86db554f764
Roland Csosz <roland@iot-inspector.com>
2021-06-18 17:30:06 +0200

    Moved DTO related parts from MarketplaceService

--- src/main/java/worldofbooks/listingreportingsystem/service/MarketplaceService.java
+++ src/main/java/worldofbooks/listingreportingsystem/service/MarketplaceService.java
@@ -1,64 +1,42 @@
 package worldofbooks.listingreportingsystem.service;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import worldofbooks.listingreportingsystem.dao.externalapi.MarketplaceAPIDTO;
-import worldofbooks.listingreportingsystem.dao.repository.MarketplaceRepository;
+import worldofbooks.listingreportingsystem.dao.repository.database.MarketplaceDbRepository;
+import worldofbooks.listingreportingsystem.dao.repository.externalapi.MarketplaceApiRepository;
 import worldofbooks.listingreportingsystem.model.entity.Marketplace;
-import worldofbooks.listingreportingsystem.util.StringConverterUtil;
 
 import java.util.List;
 
 public class MarketplaceService {
 
-    private MarketplaceRepository marketplaceRepository;
-    private List<Marketplace> fetchedMarketplaces;
-    private MarketplaceAPIDTO marketplaceAPIDTO;
+    private MarketplaceDbRepository marketplaceDBRepository;
+    private MarketplaceApiRepository marketplaceAPIRepository;
 
-    public MarketplaceService(MarketplaceRepository newMarketplaceRepository,
-                              MarketplaceAPIDTO newMarketplaceAPIDTO) {
+    public MarketplaceService(MarketplaceDbRepository newMarketplaceDbRepository,
+                              MarketplaceApiRepository newMarketplaceApiRepository) {
 
-        this.setMarketplaceRepository(newMarketplaceRepository);
-        this.setMarketplaceAPIDTO(newMarketplaceAPIDTO);
+        this.marketplaceDBRepository = newMarketplaceDbRepository;
+        this.marketplaceAPIRepository = newMarketplaceApiRepository;
     }
 
     public void fetchAndSaveData() {
-        String fetchMarketplaceData = this.marketplaceAPIDTO.fetchData();
-        this.setFetchedMarketplaces(this.getMarketplaceListFromJson(fetchMarketplaceData));
-        this.saveMarketplaceListElements(this.getFetchedMarketplaces());
+        this.saveMarketplaceListToDB(this.marketplaceAPIRepository.getAllMarketplaces());
     }
 
-    public List<Marketplace> getMarketplaceListFromJson(String json) {
-        return StringConverterUtil.convertJsonStringToGivenType(
-                json,
-                new TypeReference<List<Marketplace>>() {});
-    }
-
-    public void saveMarketplaceListElements(List<Marketplace> marketplaces){
+    public void saveMarketplaceListToDB(List<Marketplace> marketplaces){
         for(Marketplace marketplace : marketplaces){
-            marketplaceRepository.saveMarketplace(marketplace);
+            marketplaceDBRepository.saveMarketplace(marketplace);
         }
     }
 
-    public Marketplace getMarketplaceByIdFromFetchedData(int searchedId) {
-        return fetchedMarketplaces.stream()
-                .filter(marketplace -> marketplace.getId() == searchedId)
-                .findFirst()
-                .orElse(null);
+    public Marketplace getMarketplaceByIdFromAPI(int id) {
+        return this.marketplaceAPIRepository.getMarketplaceById(id);
     }
 
-    public void setMarketplaceRepository(MarketplaceRepository marketplaceRepository) {
-        this.marketplaceRepository = marketplaceRepository;
+    public void setMarketplaceDBRepository(MarketplaceDbRepository marketplaceDBRepository) {
+        this.marketplaceDBRepository = marketplaceDBRepository;
     }
 
-    public void setFetchedMarketplaces(List<Marketplace> fetchedMarketplaces) {
-        this.fetchedMarketplaces = fetchedMarketplaces;
-    }
-
-    public List<Marketplace> getFetchedMarketplaces() {
-        return fetchedMarketplaces;
-    }
-
-    public void setMarketplaceAPIDTO(MarketplaceAPIDTO marketplaceAPIDTO) {
-        this.marketplaceAPIDTO = marketplaceAPIDTO;
+    public void setMarketplaceAPIRepository(MarketplaceApiRepository marketplaceAPIRepository) {
+        this.marketplaceAPIRepository = marketplaceAPIRepository;
     }
 }
