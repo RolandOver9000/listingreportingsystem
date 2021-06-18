3c7bc278a1f777347b10ec855318209689bd5aa5
Roland Csosz <roland@iot-inspector.com>
2021-06-18 17:27:48 +0200

    Moved DAO related data from ListingService

--- src/main/java/worldofbooks/listingreportingsystem/service/ListingService.java
+++ src/main/java/worldofbooks/listingreportingsystem/service/ListingService.java
@@ -1,11 +1,10 @@
 package worldofbooks.listingreportingsystem.service;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import worldofbooks.listingreportingsystem.dao.externalapi.ListingAPIDAO;
-import worldofbooks.listingreportingsystem.dao.repository.ListingRepository;
+import worldofbooks.listingreportingsystem.dao.repository.database.ListingDbRepository;
+import worldofbooks.listingreportingsystem.dao.repository.externalapi.ListingApiRepository;
 import worldofbooks.listingreportingsystem.model.dto.ListingIncomingDTO;
 import worldofbooks.listingreportingsystem.model.entity.Listing;
-import worldofbooks.listingreportingsystem.service.validation.ListingValidationService;
+import worldofbooks.listingreportingsystem.service.validation.IncomingListingValidationService;
 import worldofbooks.listingreportingsystem.util.StringConverterUtil;
 
 import java.text.SimpleDateFormat;
@@ -13,45 +12,42 @@ import java.util.List;
 
 public class ListingService {
 
-    private ListingRepository listingRepository;
-    private List<ListingIncomingDTO> fetchedListings;
+    private ListingDbRepository listingDBRepository;
+    private ListingApiRepository listingAPIRepository;
+    private final LocationService locationService;
+    private final ListingStatusService listingStatusService;
+    private final MarketplaceService marketplaceService;
+    private final IncomingListingValidationService incomingListingValidationService;
     private final SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
-    private LocationService locationService;
-    private ListingStatusService listingStatusService;
-    private MarketplaceService marketplaceService;
-    private ListingValidationService listingValidationService;
-    private ListingAPIDAO listingAPIDAO;
 
-    public ListingService(ListingRepository newListingRepository,
+    public ListingService(ListingDbRepository newListingDbRepository,
                           LocationService newLocationService,
                           ListingStatusService newListingStatusService,
                           MarketplaceService newMarketplaceService,
-                          ListingValidationService newListingValidationService,
-                          ListingAPIDAO newListingAPIDAO) {
+                          IncomingListingValidationService newIncomingListingValidationService,
+                          ListingApiRepository newListingAPIRepository) {
 
-        this.setListingRepository(newListingRepository);
-        this.setLocationService(newLocationService);
-        this.setListingStatusService(newListingStatusService);
-        this.setMarketplaceService(newMarketplaceService);
-        this.setListingValidationService(newListingValidationService);
-        this.setListingAPIDAO(newListingAPIDAO);
+        this.listingDBRepository = newListingDbRepository;
+        this.locationService = newLocationService;
+        this.listingStatusService = newListingStatusService;
+        this.marketplaceService = newMarketplaceService;
+        this.incomingListingValidationService = newIncomingListingValidationService;
+        this.listingAPIRepository = newListingAPIRepository;
     }
 
     public void fetchAndSaveValidData() {
-        String fetchedListingData = this.listingAPIDAO.fetchData();
-        this.setFetchedListings(this.getListingListFromJson(fetchedListingData));
-        this.setFetchedListings(this.listingValidationService.getValidatedIncomingListings(this.fetchedListings));
-        this.saveListingListElementsFromFetchedData();
+        List<ListingIncomingDTO> incomingListings = this.listingAPIRepository.getAllListings();
+        List<ListingIncomingDTO> validatedIncomingListings =
+                this.incomingListingValidationService.getValidatedIncomingListings(incomingListings);
+        this.saveListingListElementsFromFetchedDataToDB(validatedIncomingListings);
     }
 
-    public List<ListingIncomingDTO> getListingListFromJson(String json) {
-        return StringConverterUtil.convertJsonStringToGivenType(
-                json,
-                new TypeReference<List<ListingIncomingDTO>>() {});
+    public void saveListingToDB(Listing listing){
+        listingDBRepository.saveListing(listing);
     }
 
-    public void saveListingListElementsFromFetchedData() {
-        for(ListingIncomingDTO listingIncomingDTO : fetchedListings) {
+    private void saveListingListElementsFromFetchedDataToDB(List<ListingIncomingDTO> incomingListings) {
+        for(ListingIncomingDTO listingIncomingDTO : incomingListings) {
             Listing newListing = new Listing();
             newListing.setId(listingIncomingDTO.getId());
             newListing.setTitle(listingIncomingDTO.getTitle());
@@ -63,41 +59,13 @@ public class ListingService {
             newListing.setUploadTime(listingIncomingDTO.getUpload_time() != null ?
                     StringConverterUtil.tryFormatDateFromString(listingIncomingDTO.getUpload_time(), formatter) : null);
             newListing.setLocation(this.locationService
-                    .getLocationByIdFromFetchedData(listingIncomingDTO.getLocation_id()));
+                    .getLocationByIdFromAPI(listingIncomingDTO.getLocation_id()));
             newListing.setListingStatus(this.listingStatusService
-                    .getListingStatusByIdFromFetchedData(Integer.parseInt(listingIncomingDTO.getListing_status())));
+                    .getListingStatusByIdFromAPI(Integer.parseInt(listingIncomingDTO.getListing_status())));
             newListing.setMarketplace(this.marketplaceService
-                    .getMarketplaceByIdFromFetchedData(Integer.parseInt(listingIncomingDTO.getMarketplace())));
+                    .getMarketplaceByIdFromAPI(Integer.parseInt(listingIncomingDTO.getMarketplace())));
 
-            listingRepository.saveListing(newListing);
+            this.saveListingToDB(newListing);
         }
-    }
-
-    public void setListingRepository(ListingRepository listingRepository) {
-        this.listingRepository = listingRepository;
-    }
-
-    public void setFetchedListings(List<ListingIncomingDTO> fetchedListings) {
-        this.fetchedListings = fetchedListings;
-    }
-
-    public void setLocationService(LocationService locationService) {
-        this.locationService = locationService;
-    }
-
-    public void setListingStatusService(ListingStatusService listingStatusService) {
-        this.listingStatusService = listingStatusService;
-    }
-
-    public void setMarketplaceService(MarketplaceService marketplaceService) {
-        this.marketplaceService = marketplaceService;
-    }
-
-    public void setListingValidationService(ListingValidationService listingValidationService) {
-        this.listingValidationService = listingValidationService;
-    }
-
-    public void setListingAPIDAO(ListingAPIDAO listingAPIDAO) {
-        this.listingAPIDAO = listingAPIDAO;
     }
 }
