18a1223751d62e1312d8c5dc945ebda1aace5780
Roland Csosz <roland@iot-inspector.com>
2021-06-28 10:08:04 +0200

    Added date formatter

--- src/main/java/worldofbooks/listingreportingsystem/util/StringConverterUtil.java
+++ src/main/java/worldofbooks/listingreportingsystem/util/StringConverterUtil.java
@@ -1,12 +1,18 @@
 package worldofbooks.listingreportingsystem.util;
 
+import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.json.JSONObject;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
+import java.util.Map;
 
 public class StringConverterUtil {
 
@@ -21,10 +27,10 @@ public class StringConverterUtil {
         return null;
     }
 
-    public static Date tryFormatDateFromString(String stringDate, SimpleDateFormat targetFormat) {
+    public static LocalDate tryFormatDateFromString(String stringDate, DateTimeFormatter targetFormat) {
         try {
-            return targetFormat.parse(stringDate);
-        } catch (ParseException e) {
+            return LocalDate.parse(stringDate, targetFormat);
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return null;
